b19d0a43926a762b911425edb4d8cea5b3ee7d21
Medwebdev <mtakisnani09@gmail.com>
2022-10-31 18:29:36 +0100

    added deployment files

---
+++ config/production.ts
@@ -0,0 +1,10 @@
+export default {
+    port: 1337,
+    databaseUri: '',
+    accessTokenTtl: '15m',
+    refreshTokenTtl: '1y',
+    accessTokenPrivateKey: '',
+    refreshTokenPrivateKey: '',
+    accessTokenPublicKey: '',
+    refreshTokenPublicKey: ''
+}
